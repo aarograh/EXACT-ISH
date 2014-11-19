5dca1b3f544ee4d53022328116c02a2a2e917fe4
Aaron Graham <aarograh@umich.edu>
2014-11-19 00:20:06 -0500

    Add sweeper.f90 file to repository
    This file currently holds a series of comments about information that
    needs to be dumped from mpact and routines that need to be mimicked
    to perform the same transport sweeps.
    
    Eventually this routine will hold the transport sweeper object and
    methods.

---
+++ sweeper.f90
@@ -0,0 +1,25 @@
+MODULE sweeper
+!TODO: Check out initExtSource, computMGFS, and updateInScatter routines in FixedSrcSolver/f90:876-879
+!TODO: Also check out setExtSource in FixedSrcSolver.f90:897
+!TODO: call sweep
+!TODO: check MOCSweeper_P0.f90:468-477
+!TODO: CALL sweep2D_prodquad
+!TODO: dump the following:
+!  nreg
+!  iangstt,iangstp
+!  azimuthal and polar weights
+!  longRayDat%nlongrays
+!  longRayDat%anglaes%longrays%ifirstModMesh, %iside, %BCIndex, %iside
+!  myModMesh%imesh%ifrstfsreg
+!  trmesh%rtdat%angles%%rays%nseg
+!  rtmesh%rtdat%angles%rays%ireg
+!  xstr
+!  rtmesh%rtdat%angles%rays%hseg
+!  modRayDat%angles%rays%nextsurf
+!  modRayDat%angles%rays%nextray
+!  myModMesh%neigh
+!TODO: look into expoa and expob
+!TODO: mimic sweep loops
+!TODO: Look into UpdateBC%Start
+!TODO: Look into final line of sweep2D_prodquad_P0 routine
+END MODULE sweeper
