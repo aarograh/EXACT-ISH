6ec3a35f6ccc03495c099a8a49b94fc723528eeb
Aaron Graham <aarograh@umich.edu>
2014-11-21 21:20:18 -0500

    Added more skeleton code and filled some routines
    Added ScatMatType, XSMeshType.  Filled in code for initExtSource_P0,
    computeMGFS_P0, updateInScatter_P0.  Added declarations for expoa,
    expob; both are unallocated for now.  Added initExtSource method
    to SourceType and SourceType_P0.  Added rsinpolang to AngQuadType.

--- sweeperUtils.f90
+++ sweeperUtils.f90
@@ -6,6 +6,9 @@ MODULE sweeperUtils
   PUBLIC :: ModularRayType
   PUBLIC :: CoreLongRayType
   PUBLIC :: ModMeshRayPtrArryType
+  PUBLIC :: XSMeshType
+  PUBLIC :: expoa
+  PUBLIC :: expob
 
   TYPE :: AngFluxBCFace
     SEQUENCE
@@ -30,13 +33,16 @@ MODULE sweeperUtils
     DOUBLE PRECISION,POINTER :: qext(:) => NULL()
     DOUBLE PRECISION,POINTER :: split(:) => NULL()
     DOUBLE PRECISION,POINTER :: qextmg(:,:) => NULL()
-!    TYPE(XSMeshType),POINTER :: myXSMesh(:) => NULL()
+    TYPE(XSMeshType),POINTER :: myXSMesh(:) => NULL()
+    CONTAINS
+      PROCEDURE(absintfc_initExtSource),PASS,DEFERRED :: initExtSource
   END TYPE SourceType
 
   TYPE,EXTENDS(SourceType) :: SourceType_P0
     DOUBLE PRECISION,POINTER :: qi1g(:)
     CONTAINS
       PROCEDURE,PASS :: updateSelfScatter_P0
+      PROCEDURE,PASS :: initExtSource => initExtSource_P0
   END TYPE SourceType_P0
 
   TYPE :: ModMeshType
@@ -59,6 +65,7 @@ MODULE sweeperUtils
     DOUBLE PRECISION,ALLOCATABLE :: walpha(:)
     DOUBLE PRECISION,ALLOCATABLE :: wtheta(:)
     DOUBLE PRECISION,ALLOCATABLE :: sinpolang(:)
+    DOUBLE PRECISION,ALLOCATABLE :: rsinpolang(:)
   END TYPE AngQuadType
 
   TYPE :: ModularRayType
@@ -103,6 +110,31 @@ MODULE sweeperUtils
     TYPE(ModMeshRayType),POINTER :: rtdat => NULL()
   END TYPE ModMeshRayPtrArryType
 
+  TYPE :: ScatMatType
+    SEQUENCE
+    INTEGER :: gmin
+    INTEGER :: gmax
+    DOUBLE PRECISION,ALLOCATABLE :: from(:)
+  END TYPE ScatMatType
+
+  TYPE :: XSMeshType
+    INTEGER :: nreg=0
+    INTEGER,ALLOCATABLE :: ireg(:)
+    DOUBLE PRECISION,ALLOCATABLE :: xsmactr(:)
+    DOUBLE PRECISION,ALLOCATABLE :: xsmacchi(:)
+    TYPE(ScatMatType),ALLOCATABLE :: xsmacsc(:,:)
+  END TYPE XSMeshType
+
+  DOUBLE PRECISION,ALLOCATABLE :: expoa(:,:),expob(:,:)
+
+  ABSTRACT INTERFACE
+    SUBROUTINE absintfc_initExtSource(thisSrc,ig)
+      IMPORT :: Sourcetype
+      CLASS(SourceType),INTENT(INOUT) :: thisSrc
+      INTEGER,INTENT(IN) :: ig
+    END SUBROUTINE absintfc_initExtSource 
+  END INTERFACE
+
   CONTAINS
 !===============================================================================
 !TODO: MOCSweeper_P0.f90:47
@@ -112,5 +144,59 @@ MODULE sweeperUtils
       DOUBLE PRECISION,INTENT(INOUT) :: qbar(:)
       DOUBLE PRECISION,INTENT(IN) :: phis1g
     END SUBROUTINE updateSelfScatter_P0
+!===============================================================================
+    SUBROUTINE initExtSource_P0(thisSrc,ig)
+      CLASS(SourceType_P0),INTENT(INOUT) :: thisSrc
+      INTEGER,INTENT(IN) :: ig
 
+      thisSrc%qi1g=thisSrc%qextmg(:,ig)
+
+    END SUBROUTINE initExtSource_P0
+!===============================================================================
+    SUBROUTINE computeMGFS_P0(thisSrc,ig,psi)
+      CLASS(SourceType_P0),INTENT(INOUT) :: thisSrc
+      INTEGER,INTENT(IN) :: ig
+      DOUBLE PRECISION,INTENT(IN) :: psi(:)
+      ! Local variables
+      INTEGER :: ix,ixo,ir,ireg
+      DOUBLE PRECISION :: chireg
+
+      DO ix=1,thisSrc%nxsreg
+        IF(ALLOCATED(thisSrc%myXSMesh(ix)%xsmacchi)) THEN
+          chireg=thisSrc%myXSMesh(ix)%xsmacchi(ig)
+          DO ir=1,thisSrc%myXSMesh(ix)%nreg
+            ireg=thisSrc%myXSMesh(ix)%ireg(ir)
+            thisSrc%qi1g(ireg)=thisSrc%qi1g(ireg)+psi(ireg)*chireg
+          ENDDO
+        ENDIF
+      ENDDO
+
+    END SUBROUTINE computeMGFS_P0
+!===============================================================================
+    SUBROUTINE updateInScatter_P0(thisSrc,ig,igstt,igstp)
+      CLASS(SourceType_P0),INTENT(INOUT) :: thisSrc
+      INTEGER,INTENT(IN) :: ig
+      INTEGER,INTENT(IN) :: igstt
+      INTEGER,INTENT(IN) :: igstp
+      ! Local Variables
+      INTEGER :: ix,ig2,ireg,ir
+      DOUBLE PRECISION :: xss_ig2_to_ig
+
+      DO ix=1,thisSrc%nxsreg
+        DO ig2=1,thisSrc%ng
+          IF(igstt <= ig2 .AND. ig2 <= igstp) THEN
+            IF(thisSrc%myXSMesh(ix)%xsmacsc(ig,0)%gmin <= ig2 .AND. &
+              ig2 <= thisSrc%myXSMesh(ix)%xsmacsc(ig,0)%gmax .AND. ig /= ig2) THEN
+              xss_ig2_to_ig=thisSrc%myXSMesh(ix)%xsmacsc(ig,0)%from(ig2)
+              DO ir=1,thisSrc%myXSMesh(ix)%nreg
+                ireg=thisSrc%myXSMesh(ix)%ireg(ir)
+                thisSrc%qi1g(ireg)=thisSrc%qi1g(ireg)+ &
+                  xss_ig2_to_ig*thisSrc%phis(ireg,ig2)
+              ENDDO
+            ENDIF
+          ENDIF
+        ENDDO
+      ENDDO
+
+    END SUBROUTINE updateInScatter_P0 
 END MODULE sweeperUtils
