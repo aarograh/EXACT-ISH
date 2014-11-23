a4c2ed9e5d1eceeec2a2a63ed66ef8cba4301063
Aaron Graham <aarograh@umich.edu>
2014-11-23 01:27:59 -0500

    Added calls to ProcessCmdLine and closeFiles

--- EXACT-ISH.f90
+++ EXACT-ISH.f90
@@ -3,6 +3,24 @@ PROGRAM EXACTISH
   USE sweeper
   USE IO
 
+  WRITE(*,*) '======================================'
+  WRITE(*,*) 'Processing command line arguments...'
+  WRITE(*,*) '======================================'
+  CALL processCmdLine()
+
+  WRITE(*,*) '======================================'
+  WRITE(*,*) 'Parsing data and setting up solvers...'
+  WRITE(*,*) '======================================'
+
+  WRITE(*,*) '======================================'
+  WRITE(*,*) 'Performing transport sweeps...'
+  WRITE(*,*) '======================================'
+
+  WRITE(*,*) '======================================'
+  WRITE(*,*) 'Closing files...'
+  WRITE(*,*) '======================================'
+  CALL closeFiles()
+
   WRITE(*,*) 'Hello World'
 
 END PROGRAM EXACTISH
