65e602320900e12fc3ac11a030c22f1f20917803
Aaron Graham <aarograh@umich.edu>
2014-11-23 01:27:09 -0500

    Added ProcessCmdLine and closeFiles routines to IO

--- IO.f90
+++ IO.f90
@@ -2,5 +2,28 @@ MODULE IO
 
   USE sweeper
 
+  IMPLICIT NONE
+
+  PUBLIC :: processCmdLine
+  PUBLIC :: closeFiles
+
+  INTEGER,PARAMETER :: inpFileUnitNo=123
+
+  CONTAINS
+!===============================================================================
+    SUBROUTINE processCmdLine()
+      ! Local Variables
+      CHARACTER(LEN=16) :: arg_in
+
+      CALL GET_COMMAND_ARGUMENT(1,arg_in)
+      OPEN(FILE=TRIM(ADJUSTL(arg_in)),UNIT=inpFileUnitNo)
+
+    END SUBROUTINE processCmdLine
+!===============================================================================
+    SUBROUTINE closeFiles()
+
+      CLOSE(UNIT=inpFileUnitNo)
+
+    END SUBROUTINE closeFiles
 END MODULE IO
 
