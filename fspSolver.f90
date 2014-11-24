1e4f9daea1cbab725a6e9fbfa1176d1adf0d577c
Aaron Graham <aarograh@umich.edu>
2014-11-24 12:34:10 -0500

    Add skeleton for fspSolverType and initialize() routine

--- fspSolver.f90
+++ fspSolver.f90
@@ -1,4 +1,25 @@
 MODULE fspSolver
 
+  USE sweeper
 
+  PUBLIC :: fspSolverType
+
+  TYPE :: fspSolverType
+    INTEGER :: igstt = 0
+    INTEGER :: igstp = 0
+    CLASS(SourceType),POINTER :: thisSource
+    CLASS(sweeperType),POINTER :: thisSweeper => NULL()
+    CONTAINS
+      PROCEDURE,PASS :: initialize
+  END TYPE fspSolverType
+
+  CONTAINS
+!===============================================================================
+    SUBROUTINE initialize(mySolver,thisSweeper)
+      CLASS(fspSolverType),INTENT(INOUT) :: mySolver
+      CLASS(sweeperType),TARGET,INTENT(INOUT) :: thisSweeper
+
+
+
+    END SUBROUTINE initialize
 END MODULE fspSolver
