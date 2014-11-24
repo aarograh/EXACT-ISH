ce68abe6c8bccb1b6dd8ec0cf70069d9f5cf750e
Aaron Graham <aarograh@umich.edu>
2014-11-24 14:52:40 -0500

    Add step method to fspSolver

--- fspSolver.f90
+++ fspSolver.f90
@@ -2,15 +2,19 @@ MODULE fspSolver
 
   USE sweeper
 
+  IMPLICIT NONE
+
   PUBLIC :: fspSolverType
 
   TYPE :: fspSolverType
     INTEGER :: igstt = 0
     INTEGER :: igstp = 0
+    DOUBLE PRECISION,POINTER :: psi(:) => NULL()
     CLASS(SourceType),POINTER :: thisSource
     CLASS(sweeperType),POINTER :: thisSweeper => NULL()
     CONTAINS
       PROCEDURE,PASS :: initialize => initializeFspSolver
+      PROCEDURE,PASS :: step => stepFspSolver
   END TYPE fspSolverType
 
   CONTAINS
@@ -26,4 +30,21 @@ MODULE fspSolver
       CALL thisSweeper%initialize(mySolver%thisSource)
 
     END SUBROUTINE initializeFspSolver
+!===============================================================================
+    SUBROUTINE stepFspSolver(mySolver)
+      CLASS(fspSolverType),INTENT(INOUT) :: mySolver
+      ! Local Variables
+      INTEGER :: ig
+
+      DO ig=mySolver%igstt,mySolver%igstp
+        ! Set up source
+        CALL mySolver%thisSource%initExtSource(ig)
+        CALL mySolver%thisSource%computeMGFS(ig,mySolver%psi)
+        CALL mySolver%thisSource%updateInScatter(ig,mySolver%igstt,mySolver%igstp)
+        CALL mySolver%thisSweeper%setExtSource(mySolver%thisSource)
+        ! Perform sweep
+        CALL mySolver%thisSweeper%sweep(ig,3,1.0D-03)
+      ENDDO
+
+    END SUBROUTINE stepFspSolver
 END MODULE fspSolver
