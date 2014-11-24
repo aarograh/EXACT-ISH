1095d90388c6620f2c28ee7b0b5e3d9b6b2ef674
Aaron Graham <aarograh@umich.edu>
2014-11-24 16:35:33 -0500

    Added reading of phis and BC data.  Also made some style updates

--- IO.f90
+++ IO.f90
@@ -27,124 +27,147 @@ MODULE IO
 
     END SUBROUTINE closeFiles
 !===============================================================================
-    SUBROUTINE populateData(mySweeper)
-      TYPE(sweeperType),INTENT(INOUT) :: mySweeper
+    SUBROUTINE populateData(sweeper)
+      CLASS(sweeperType),INTENT(INOUT) :: sweeper
       ! Local variables
-      INTEGER :: i,ii,iii,n1,n2,n3,n4
+      INTEGER :: i,ii,iii,n1,n2,n3,n4,n5
 
       ! Read number of groups and size of XS mesh
       READ(123,*) n1,n2,n3
-      mySweeper%ngroups=n3-n2+1
-      mySweeper%igstt=n2
-      mySweeper%igstp=n3
-      ALLOCATE(mySweeper%myXSMesh(n1))
+      sweeper%ng=n3-n2+1
+      sweeper%igstt=n2
+      sweeper%igstp=n3
+      ALLOCATE(sweeper%myXSMesh(n1))
 
       ! Read XSMeshType data
       DO i=1,n1
-        READ(123,*) mySweeper%myXSMesh(i)%nreg
-        ALLOCATE(mySweeper%myXSMesh(i)%ireg(mySweeper%myXSMesh(i)%nreg))
-        READ(123,*) mySweeper%myXSMesh(i)%ireg
-        ALLOCATE(mySweeper%myXSMesh(i)%xsmactr(mySweeper%ngroups))
-        READ(123,*) mySweeper%myXSMesh(i)%xsmactr
-        ALLOCATE(mySweeper%myXSMesh(i)%xsmacchi(mySweeper%ngroups))
-        READ(123,*) mySweeper%myXSMesh(i)%xsmacchi
-        ALLOCATE(mySweeper%myXSMesh(i)%xsmacsc(mySweeper%ngroups,0:0))
-        DO ii=1,mySweeper%ngroups
-          READ(123,*) mySweeper%myXSMesh(i)%xsmacsc(ii,0)%gmin
-          READ(123,*) mySweeper%myXSMesh(i)%xsmacsc(ii,0)%gmax
-          ALLOCATE(mySweeper%myXSMesh(i)%xsmacsc(ii,0)%from( &
-            mySweeper%myXSMesh(i)%xsmacsc(ii,0)%gmin: &
-            mySweeper%myXSMesh(i)%xsmacsc(ii,0)%gmax))
-          READ(123,*) mySweeper%myXSMesh(i)%xsmacsc(ii,0)%from
+        READ(123,*) sweeper%myXSMesh(i)%nreg
+        ALLOCATE(sweeper%myXSMesh(i)%ireg(sweeper%myXSMesh(i)%nreg))
+        READ(123,*) sweeper%myXSMesh(i)%ireg
+        ALLOCATE(sweeper%myXSMesh(i)%xsmactr(sweeper%ng))
+        READ(123,*) sweeper%myXSMesh(i)%xsmactr
+        ALLOCATE(sweeper%myXSMesh(i)%xsmacchi(sweeper%ng))
+        READ(123,*) sweeper%myXSMesh(i)%xsmacchi
+        ALLOCATE(sweeper%myXSMesh(i)%xsmacsc(sweeper%ng,0:0))
+        DO ii=1,sweeper%ng
+          READ(123,*) sweeper%myXSMesh(i)%xsmacsc(ii,0)%gmin
+          READ(123,*) sweeper%myXSMesh(i)%xsmacsc(ii,0)%gmax
+          ALLOCATE(sweeper%myXSMesh(i)%xsmacsc(ii,0)%from( &
+            sweeper%myXSMesh(i)%xsmacsc(ii,0)%gmin: &
+            sweeper%myXSMesh(i)%xsmacsc(ii,0)%gmax))
+          READ(123,*) sweeper%myXSMesh(i)%xsmacsc(ii,0)%from
         ENDDO
       ENDDO
 
       ! Read ModMeshType data
-      ALLOCATE(ModMeshType :: mySweeper%myModMesh)
-      READ(123,*) mySweeper%myModMesh%nmesh
-      ALLOCATE(mySweeper%myModMesh%ifrstfsreg(mySweeper%myModMesh%nmesh))
-      DO i=1,mySweeper%myModMesh%nmesh
-        READ(123,*) mySweeper%myModMesh%ifrstfsreg(i)
+      ALLOCATE(ModMeshType :: sweeper%myModMesh)
+      READ(123,*) sweeper%myModMesh%nmesh
+      ALLOCATE(sweeper%myModMesh%ifrstfsreg(sweeper%myModMesh%nmesh))
+      DO i=1,sweeper%myModMesh%nmesh
+        READ(123,*) sweeper%myModMesh%ifrstfsreg(i)
       ENDDO
       READ(123,*) n1,n2
-      ALLOCATE(mySweeper%myModMesh%neigh(n1,n2))
-      READ(123,*) mySweeper%myModMesh%neigh
+      ALLOCATE(sweeper%myModMesh%neigh(n1,n2))
+      READ(123,*) sweeper%myModMesh%neigh
 
       ! Read ModMeshRayPtrArryType Data
       READ(123,*) n1
-      ALLOCATE(mySweeper%rtmesh(n1))
+      ALLOCATE(sweeper%rtmesh(n1))
       DO i=1,n1
         READ(123,*) n2
-        ALLOCATE(mySweeper%rtmesh(i)%rtdat)
-        ALLOCATE(mySweeper%rtmesh(i)%rtdat%angles(n2))
+        ALLOCATE(sweeper%rtmesh(i)%rtdat)
+        ALLOCATE(sweeper%rtmesh(i)%rtdat%angles(n2))
         DO ii=1,n2
           READ(123,*) n3
-          ALLOCATE(mySweeper%rtmesh(i)%rtdat%angles(ii)%rays(n3))
+          ALLOCATE(sweeper%rtmesh(i)%rtdat%angles(ii)%rays(n3))
           DO iii=1,n3
             READ(123,*) n4
-            ALLOCATE(mySweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%ireg(n4))
-            READ(123,*) mySweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%ireg
+            ALLOCATE(sweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%ireg(n4))
+            READ(123,*) sweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%ireg
             READ(123,*) n4
-            ALLOCATE(mySweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%hseg(n4))
-            READ(123,*) mySweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%hseg
+            ALLOCATE(sweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%hseg(n4))
+            READ(123,*) sweeper%rtmesh(i)%rtdat%angles(ii)%rays(iii)%hseg
           ENDDO
         ENDDO
       ENDDO
 
       ! Read CoreLongRays data
       READ(123,*) n1
-      ALLOCATE(mySweeper%longRayDat%nlongrays(n1))
-      ALLOCATE(mySweeper%longRayDat%angles(n1))
+      ALLOCATE(sweeper%longRayDat%nlongrays(n1))
+      ALLOCATE(sweeper%longRayDat%angles(n1))
       DO i=1,n1
-        READ(123,*) mySweeper%longRayDat%nlongrays(i)
-        ALLOCATE(mySweeper%longRayDat%angles(i)%longrays( &
-          mySweeper%longRayDat%nlongrays(i)))
-        DO ii=1,mySweeper%longRayDat%nlongrays(i)
-          READ(123,*) mySweeper%longRayDat%angles(i)%longrays(ii)%ifirstModMesh
-          READ(123,*) mySweeper%longRayDat%angles(i)%longrays(ii)%iside
-          READ(123,*) mySweeper%longRayDat%angles(i)%longrays(ii)%firstModRay
-          READ(123,*) mySweeper%longRayDat%angles(i)%longrays(ii)%BCIndex
+        READ(123,*) sweeper%longRayDat%nlongrays(i)
+        ALLOCATE(sweeper%longRayDat%angles(i)%longrays( &
+          sweeper%longRayDat%nlongrays(i)))
+        DO ii=1,sweeper%longRayDat%nlongrays(i)
+          READ(123,*) sweeper%longRayDat%angles(i)%longrays(ii)%ifirstModMesh
+          READ(123,*) sweeper%longRayDat%angles(i)%longrays(ii)%iside
+          READ(123,*) sweeper%longRayDat%angles(i)%longrays(ii)%firstModRay
+          READ(123,*) sweeper%longRayDat%angles(i)%longrays(ii)%BCIndex
         ENDDO
       ENDDO
 
       ! Read ModularRayType data
-      ALLOCATE(mySweeper%modRayDat)
-      READ(123,*) mySweeper%modRayDat%iangstt,mySweeper%modRayDat%iangstp
-      n1=mySweeper%modRayDat%iangstp-mySweeper%modRayDat%iangstt+1
-      ALLOCATE(mySweeper%modRayDat%angles(n1))
+      ALLOCATE(sweeper%modRayDat)
+      READ(123,*) sweeper%modRayDat%iangstt,sweeper%modRayDat%iangstp
+      n1=sweeper%modRayDat%iangstp-sweeper%modRayDat%iangstt+1
+      ALLOCATE(sweeper%modRayDat%angles(n1))
       DO i=1,n1
-        READ(123,*) mySweeper%modRayDat%angles(i)%dlr
+        READ(123,*) sweeper%modRayDat%angles(i)%dlr
         READ(123,*) n2
-        mySweeper%modRayDat%angles(i)%nmodrays=n2
-        ALLOCATE(mySweeper%modRayDat%angles(i)%rays(n2))
+        sweeper%modRayDat%angles(i)%nmodrays=n2
+        ALLOCATE(sweeper%modRayDat%angles(i)%rays(n2))
         DO ii=1,n2
-          READ(123,*) mySweeper%modRayDat%angles(i)%rays(ii)%nextray
-          READ(123,*) mySweeper%modRayDat%angles(i)%rays(ii)%nextsurf
+          READ(123,*) sweeper%modRayDat%angles(i)%rays(ii)%nextray
+          READ(123,*) sweeper%modRayDat%angles(i)%rays(ii)%nextsurf
         ENDDO
       ENDDO
 
       ! Read Quadrature information
       READ(123,*) n1
-      ALLOCATE(mySweeper%modRayDat%angquad%walpha(n1))
-      READ(123,*) mySweeper%modRayDat%angquad%walpha        
+      ALLOCATE(sweeper%modRayDat%angquad%walpha(n1))
+      READ(123,*) sweeper%modRayDat%angquad%walpha        
       READ(123,*) n1
-      ALLOCATE(mySweeper%modRayDat%angquad%wtheta(n1))
-      READ(123,*) mySweeper%modRayDat%angquad%wtheta
+      ALLOCATE(sweeper%modRayDat%angquad%wtheta(n1))
+      READ(123,*) sweeper%modRayDat%angquad%wtheta
       READ(123,*) n1
-      ALLOCATE(mySweeper%modRayDat%angquad%sinpolang(n1))
-      READ(123,*) mySweeper%modRayDat%angquad%sinpolang       
-      ALLOCATE(mySweeper%modRayDat%angquad%rsinpolang(n1))
-      READ(123,*) mySweeper%modRayDat%angquad%rsinpolang       
+      ALLOCATE(sweeper%modRayDat%angquad%sinpolang(n1))
+      READ(123,*) sweeper%modRayDat%angquad%sinpolang       
+      ALLOCATE(sweeper%modRayDat%angquad%rsinpolang(n1))
+      READ(123,*) sweeper%modRayDat%angquad%rsinpolang       
 
       ! Read pz data
-      READ(123,*) mySweeper%pz
+      READ(123,*) sweeper%pz
 
       ! Read Exponential Tables data
-      ALLOCATE(mySweeper%expTableDat)
+      ALLOCATE(sweeper%expTableDat)
       READ(123,*) n1
       READ(123,*) n2
-      ALLOCATE(mySweeper%expTableDat%table2D(1:2,n1:n2))
-      READ(123,*) mySweeper%expTableDat%table2D
+      ALLOCATE(sweeper%expTableDat%table2D(1:2,n1:n2))
+      READ(123,*) sweeper%expTableDat%table2D
+
+      ! Read AngFluxBC data
+      READ(123,*) n1
+      ALLOCATE(sweeper%phiang(n1))
+      DO i=1,n1
+        READ(123,*) n2
+        ALLOCATE(sweeper%phiang(i)%angle(n2))
+        DO ii=1,n2
+          READ(123,*) n3
+          ALLOCATE(sweeper%phiang(i)%angle(ii)%face(n3))
+          DO iii=1,n3
+            READ(123,*) n4,n5
+            ALLOCATE(sweeper%phiang(i)%angle(ii)%face(iii)%angFlux(n4,n5))
+            READ(123,*) sweeper%phiang(i)%angle(ii)%face(iii)%angFlux
+          ENDDO
+        ENDDO
+      ENDDO
+
+      ! Read in initial phis
+      READ(123,*) n1,n2
+      sweeper%nreg = n1
+      ALLOCATE(sweeper%phis(n1,n2))
+      READ(123,*) sweeper%phis
 
     END SUBROUTINE populateData
 END MODULE IO
