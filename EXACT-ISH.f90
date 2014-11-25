cbd74539a1dd8a330eb04cf3f8fbba505248fe76
Aaron Graham <aarograh@umich.edu>
2014-11-25 03:21:44 -0500

    Added call to validate() and removed 'Hello World' message

--- EXACT-ISH.f90
+++ EXACT-ISH.f90
@@ -7,26 +7,34 @@ PROGRAM EXACTISH
 
   TYPE(fspSolverType) :: solver
 
+  WRITE(*,*)
   WRITE(*,*) '======================================'
   WRITE(*,*) 'Processing command line arguments...'
   WRITE(*,*) '======================================'
   CALL processCmdLine()
 
+  WRITE(*,*)
   WRITE(*,*) '======================================'
   WRITE(*,*) 'Initializing solvers...'
   WRITE(*,*) '======================================'
   CALL solver%initialize()
 
+  WRITE(*,*)
   WRITE(*,*) '======================================'
   WRITE(*,*) 'Performing transport sweeps...'
   WRITE(*,*) '======================================'
   CALL solver%solve()
 
+  WRITE(*,*)
+  WRITE(*,*) '======================================'
+  WRITE(*,*) 'Validating Solution...'
+  WRITE(*,*) '======================================'
+  CALL validate(solver%sweeper)
+
+  WRITE(*,*)
   WRITE(*,*) '======================================'
   WRITE(*,*) 'Closing files...'
   WRITE(*,*) '======================================'
   CALL closeFiles()
-
-  WRITE(*,*) 'Hello World'
 
 END PROGRAM EXACTISH
