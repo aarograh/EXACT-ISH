4bb4f0ecb9dc196afadc35dbccf2860d41bbd646
Aaron Graham <aarograh@umich.edu>
2014-12-15 16:39:29 -0500

    Adding OPENACCSOLVER option.  Routine is there, but exactly the same as the ENERGYINNER option right now.  Also adding WRITE statements to prevent segfaults (yeah, PGI is that stupid...)

--- openacc.f90
+++ openacc.f90
@@ -14,6 +14,7 @@ MODULE openacc
   PUBLIC :: sweep2D_prodquad_P0_PGI
   PUBLIC :: MOCSolver_Sweep1G_PGI
   PUBLIC :: MOCSolver_SweepMG_PGI
+  PUBLIC :: MOCSolver_SweepACC_PGI
 
   TYPE :: sweeperType_PGI
     LOGICAL :: hasSource=.FALSE.
@@ -120,10 +121,10 @@ MODULE openacc
       sweeper%phis1g = 0.0D0
       ALLOCATE(sweeper%phis1gd(sweeper%nreg))
       sweeper%phis1gd = 0.0D0
-      ALLOCATE(sweeper%qbar(sweeper%nreg))
-      sweeper%qbar = 0.0D0
       IF(sweepType < 12) THEN
         ALLOCATE(sweeper%xstr(sweeper%nreg))
+        ALLOCATE(sweeper%qbar(sweeper%nreg))
+        sweeper%qbar = 0.0D0
       ELSE
         ALLOCATE(sweeper%xstrmg(sweeper%nreg,sweeper%ng))
         sweeper%xstrmg = 0.0D0
@@ -400,15 +401,16 @@ MODULE openacc
         CALL computeMGFS_PGI(source,ig,psi)
       ENDDO !ig
 
+WRITE(*,*) ALLOCATED(sweeper%qbarmg)
       DO i=1,ninners
         sweeper%nsweeps = sweeper%nsweeps + 1
         !IF(i == ninners) sweeper%sweepCur=.TRUE.
         DO ig=1,sweeper%ng
           ! Update in-scatter and self-scatter source
           CALL updateInScatter_PGI(source,ig,sweeper%igstt,sweeper%igstp)
-          CALL sweeper%mySrc%updateSelfScatter(ig,sweeper%qbar,sweeper%phis1g)
+          CALL sweeper%mySrc%updateSelfScatter(ig,sweeper%qbarmg(:,ig),sweeper%phis(:,ig))
           CALL MOCSolver_Setup1GFSP_PGI(sweeper%myXSMesh,sweeper%nxsreg, &
-            sweeper%phis1g,sweeper%nreg,sweeper%xstrmg(:,ig),sweeper%qbarmg(:,ig),ig)
+            sweeper%phis(:,ig),sweeper%nreg,sweeper%xstrmg(:,ig),sweeper%qbarmg(:,ig),ig)
           sweeper%phis1gd = sweeper%phis(:,ig)
         ENDDO !ig
 
@@ -532,4 +534,169 @@ MODULE openacc
       CALL sweeper%UpdateBC%Finish()
 
     END SUBROUTINE MOCSolver_SweepMG_PGI
+!===============================================================================
+    SUBROUTINE MOCSolver_SweepACC_PGI(sweeper,ninners,tol,source,psi)
+      CLASS(sweeperType_PGI),INTENT(INOUT) :: sweeper
+      INTEGER,INTENT(IN) :: ninners
+      DOUBLE PRECISION,INTENT(IN) :: tol
+      CLASS(SourceType_PGI),POINTER,INTENT(INOUT) :: source
+      DOUBLE PRECISION,INTENT(INOUT) :: psi(:)
+      ! Local Variables
+      INTEGER :: i,ig
+      INTEGER :: iang,ipol,ilray,imray,imod,im,iside,inextsurf,ifrstreg
+      INTEGER :: imseg,iseg,iseg1,iseg2,ibc1,ibc2,nseglray,is1,is2,npol,ireg
+      INTEGER :: irg_seg(0:sweeper%maxsegray),ithd
+      INTEGER :: ifrstreg_proc,ireg1,ireg2
+      DOUBLE PRECISION :: timeStt,timeStp,timeTotal
+      DOUBLE PRECISION :: phid1,phid2,wsum,rpol
+      DOUBLE PRECISION :: wtangazi,wtang(SIZE(sweeper%modRayDat%angquad%wtheta))
+      DOUBLE PRECISION :: phio1,phio1d,phio2,phio2d
+      DOUBLE PRECISION :: tau_seg(sweeper%maxsegray)
+      DOUBLE PRECISION :: exparg(sweeper%maxsegray)
+!      DOUBLE PRECISION :: xstrA(sweeper%nreg)
+!      DOUBLE PRECISION :: qbarWsum(sweeper%nreg)
+      TYPE(LongRayType_Base) :: ilongRay
+
+      timeTotal = 0.0D0
+
+      DO ig=1,sweeper%ng
+        ! Set up external and fission source
+        source%qi1g => source%qimg(:,ig)
+        CALL initExtSource_PGI(source,ig)
+        CALL computeMGFS_PGI(source,ig,psi)
+      ENDDO !ig
+
+WRITE(*,*) ALLOCATED(sweeper%qbarmg)
+      DO i=1,ninners
+        sweeper%nsweeps = sweeper%nsweeps + 1
+        !IF(i == ninners) sweeper%sweepCur=.TRUE.
+        DO ig=1,sweeper%ng
+          ! Update in-scatter and self-scatter source
+          CALL updateInScatter_PGI(source,ig,sweeper%igstt,sweeper%igstp)
+          CALL sweeper%mySrc%updateSelfScatter(ig,sweeper%qbarmg(:,ig),sweeper%phis(:,ig))
+          CALL MOCSolver_Setup1GFSP_PGI(sweeper%myXSMesh,sweeper%nxsreg, &
+            sweeper%phis(:,ig),sweeper%nreg,sweeper%xstrmg(:,ig),sweeper%qbarmg(:,ig),ig)
+          sweeper%phis1gd = sweeper%phis(:,ig)
+        ENDDO !ig
+
+        sweeper%phis = 0.0D0
+
+        CALL CPU_TIME(timeStt)
+!-----------------------------------------------------------------------------
+        ithd = 1
+        npol = SIZE(sweeper%modRayDat%angquad%wtheta)
+        wsum = 4.0D0*PI
+        ifrstreg_proc = sweeper%myModMesh%ifrstfsreg(sweeper%imeshstt)
+          
+        DO iang=sweeper%modRayDat%iangstt,sweeper%modRayDat%iangstp
+          wtangazi = sweeper%modRayDat%angles(iang)%dlr* &
+            sweeper%modRayDat%angquad%walpha(iang)*PI
+          DO ipol=1,npol
+            wtang(ipol) = wtangazi*sweeper%modRayDat%angquad%wtheta(ipol)* &
+              sweeper%modRayDat%angquad%sinpolang(ipol)
+          ENDDO !ipol
+        
+          DO ilray=1,sweeper%longRayDat%nlongrays(iang)
+            ilongRay = sweeper%longRayDat%angles(iang)%longrays(ilray)
+            iside = ilongRay%iside(1)
+            ibc1 = ilongRay%BCIndex(1)
+            ibc2 = ilongRay%BCIndex(2)
+            is1 = ilongRay%iside(1)
+            is2 = ilongRay%iside(2)
+
+            DO ig=1,sweeper%ng
+              sweeper%xstr => sweeper%xstrmg(:,ig)
+              sweeper%qbar => sweeper%qbarmg(:,ig)
+              sweeper%activeg = ig
+              imray = ilongRay%firstModRay
+              im = ilongRay%ifirstModMesh
+              iseg = 0
+        
+              DO imod=1,ilongRay%nmods
+                ifrstreg = sweeper%myModMesh%ifrstfsreg(im)
+        
+                DO imseg=1,sweeper%rtmesh(im)%rtdat%angles(iang)%rays(imray)%nseg
+                  ireg = ifrstreg - ifrstreg_proc + &
+                    sweeper%rtmesh(im)%rtdat%angles(iang)%rays(imray)%ireg(imseg)
+                  iseg = iseg + 1
+                  tau_seg(iseg) = -sweeper%xstr(ireg)* &
+                    sweeper%rtmesh(im)%rtdat%angles(iang)%rays(imray)%hseg(imseg)
+                  irg_seg(iseg) = ireg
+                ENDDO !imseg
+        
+                inextsurf = sweeper%modRayDat%angles(iang)%rays(imray)%nextsurf(1)
+                imray = sweeper%modRayDat%angles(iang)%rays(imray)%nextray(1)
+                im = sweeper%myModMesh%neigh(inextsurf,im)
+              ENDDO !imod
+
+              nseglray = iseg
+!              xstrA = sweeper%xstr*sweeper%vol/sweeper%pz
+!              qbarWsum = sweeper%qbar*wsum
+              DO ipol=1,npol
+                rpol = sweeper%modRayDat%angquad%rsinpolang(ipol)
+                DO iseg=1,nseglray
+                  exparg(iseg) = sweeper%expTableDat%EXPT(tau_seg(iseg)*rpol)
+                ENDDO !iseg
+
+                phio1 = sweeper%phiang(ig)%angle(iang)%face(is1)%angflux(ipol,ibc1)
+                phio2 = sweeper%phiang(ig)%angle(iang)%face(is2)%angflux(ipol,ibc2)
+                iseg2 = nseglray + 1
+        
+                DO iseg1=1,nseglray
+                  phio1d = phio1
+                  phio2d = phio2
+                  iseg2 = iseg2 - 1
+          
+                  ireg1 = irg_seg(iseg1)
+                  phid1 = (phio1d - sweeper%qbar(ireg1)) * exparg(iseg1)
+                  !phio1 stores the outgoing angular flux to be used for the next
+                  !segment as incoming angular flux.
+                  phio1 = phio1d - phid1
+                  sweeper%phis(ireg1,ig) = sweeper%phis(ireg1,ig) + phid1*wtang(ipol)!/xstrA(ireg1) + qbarWsum(ireg1)
+        
+                  ireg2 = irg_seg(iseg2)
+                  phid2 = (phio2d - sweeper%qbar(ireg2)) * exparg(iseg2)
+                  !phio1 stores the outgoing angular flux to be used for the next
+                  !segment as incoming angular flux.
+                  phio2 = phio2d - phid2
+                  sweeper%phis(ireg2,ig) = sweeper%phis(ireg2,ig) + phid2*wtang(ipol)!/xstrA(ireg2) + qbarWsum(ireg2)
+                ENDDO !iseg
+        
+                sweeper%phiangmg_out(ig)%angle(iang)%face(is1)%angflux(ipol,ibc1) = phio2
+                sweeper%phiangmg_out(ig)%angle(iang)%face(is2)%angflux(ipol,ibc2) = phio1
+              ENDDO !ipol
+            ENDDO !ig
+          ENDDO !ilray
+        
+          DO ig=1,sweeper%ng
+            CALL sweeper%UpdateBC%Start(iang,sweeper%phiangmg_out(ig),sweeper%phiang(ig))
+          ENDDO
+        ENDDO !iang
+        
+        DO ig=1,sweeper%ng
+          sweeper%phis(:,ig) = sweeper%phis(:,ig)/(sweeper%xstrmg(:,ig)* &
+            sweeper%vol/sweeper%pz) + sweeper%qbarmg(:,ig)*wsum
+        ENDDO !ig
+
+        CALL CPU_TIME(timeStp)
+!-------------------------------------------------------------------------------
+        timeStp = timeStp - timeStt
+        timeTotal = timeTotal + timeStp
+        WRITE(*,*) 'Iteration Time: ',timeStp
+        WRITE(*,*) 'Accumulated Sweep Time: ',timeTotal
+        WRITE(*,*)
+      ENDDO !i
+
+      ! Write to output file for comparison
+      WRITE(125,*) SHAPE(sweeper%phis)
+      DO ig=1,sweeper%ng
+        DO i=1,sweeper%nreg
+          WRITE(125,*) sweeper%phis(i,ig)
+        ENDDO
+      ENDDO
+
+      ! Update boundary surface flux here, if sweep Cur and associated coarse mesh
+      CALL sweeper%UpdateBC%Finish()
+
+    END SUBROUTINE MOCSolver_SweepACC_PGI
 END MODULE openacc
