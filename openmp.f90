b6b18e34557e05345624ba1493b6741bf1300286
Jipu Wang <jipuwang@umich.edu>
2014-12-28 15:07:16 -0500

    clean up some comments so as to start the work after the projects

--- openmp.f90
+++ openmp.f90
@@ -6,13 +6,19 @@ MODULE openmp
   IMPLICIT NONE
   PRIVATE
 
+  !This is the same as the base sweeper
   PUBLIC :: sweep2D_prodquad_P0_vectoripol0
+  !This version vectorize the first block
   PUBLIC :: sweep2D_prodquad_P0_vectoripol1
+  !Inline the polar angle dependent table loopup
   PUBLIC :: sweep2D_prodquad_P0_vectoripol1_2
+  !Vectorize the second block
   PUBLIC :: sweep2D_prodquad_P0_vectoripol2
-!   PUBLIC :: sweep2D_prodquad_P0_vectoripol2_2
+  !Vectorize the third blcok
   PUBLIC :: sweep2D_prodquad_P0_vectoripol3
+  !Vectorize all three blocks with inlined expT lookup - best performance
   PUBLIC :: sweep2D_prodquad_P0_vectoripol3_2
+  !With OpenMP
   PUBLIC :: sweep2D_prodquad_P0_vectoripol3_3
 
   CONTAINS
@@ -141,7 +147,7 @@ MODULE openmp
       sweeper%phis1g = sweeper%phis1g/(sweeper%xstr*sweeper%vol/sweeper%pz) + &
         sweeper%qbar*wsum
 
-    END SUBROUTINE sweep2D_prodquad_P0_vectoripol0
+    ENDSUBROUTINE sweep2D_prodquad_P0_vectoripol0
 !===============================================================================
     !This version vectorize the first block
     SUBROUTINE sweep2D_prodquad_P0_vectoripol1(sweeper,i)
@@ -508,7 +514,7 @@ MODULE openmp
 
           nseglray = iseg
 
-! !the following blcok uses scalar lookup, but one sweep takes 0.2720 sec.
+! !the following blcok uses scalar lookup.
 !           DO ipol=1,npol
 !             rpol = sweeper%modRayDat%angquad%rsinpolang(ipol)
 !             DO iseg=1,nseglray
@@ -516,7 +522,7 @@ MODULE openmp
 !             ENDDO !iseg
 !           ENDDO !ipol
 
-!the following block uses vector fetch, but one sweep now takes 0.11 sec.
+!the following block uses vector fetch.
           ALLOCATE(arg(npol))
           DO iseg=1,nseglray
             arg=tau_seg(iseg)*sweeper%modRayDat%angquad%rsinpolang
@@ -612,18 +618,10 @@ MODULE openmp
       DO iang=sweeper%modRayDat%iangstt,sweeper%modRayDat%iangstp
         wtangazi = sweeper%modRayDat%angles(iang)%dlr* &
           sweeper%modRayDat%angquad%walpha(iang)*PI
-!         DO ipol=1,npol
-!           wtang(ipol) = wtangazi*sweeper%modRayDat%angquad%wtheta(ipol)* &
-!             sweeper%modRayDat%angquad%sinpolang(ipol)
-!         ENDDO !ipol
 
         !vectorizing the first block here
         wtang = wtangazi*sweeper%modRayDat%angquad%wtheta* &
           sweeper%modRayDat%angquad%sinpolang
-
-!         !variation 1: including the bounds
-!         wtang(1:npol) = wtangazi*sweeper%modRayDat%angquad%wtheta(1:npol)* &
-!           sweeper%modRayDat%angquad%sinpolang(1:npol)
 
         phibar = 0.0D0
 
@@ -656,7 +654,7 @@ MODULE openmp
           ENDDO !imod
 
           nseglray = iseg
-!the following blcok uses scalar lookup, but one sweep takes 0.2720 sec.
+!the following blcok uses scalar lookup.
           DO ipol=1,npol
             rpol = sweeper%modRayDat%angquad%rsinpolang(ipol)
             DO iseg=1,nseglray
@@ -664,7 +662,7 @@ MODULE openmp
             ENDDO !iseg
           ENDDO !ipol
 
-! !the following block uses vector fetch, but one sweep now takes 0.11 sec.
+! !the following block uses vector fetch.
 !           ALLOCATE(arg(npol))
 !           DO iseg=1,nseglray
 !             arg=tau_seg(iseg)*sweeper%modRayDat%angquad%rsinpolang
@@ -726,8 +724,8 @@ MODULE openmp
 
     ENDSUBROUTINE sweep2D_prodquad_P0_vectoripol3
 !===============================================================================
-    !This version vectorize the third block
-    !actually vectorize all blocks and inline the exponetial table lookup.
+    !This version vectorize all three blocks
+    !inline the exponetial table lookup.
     !This has given the best performance ever since
     SUBROUTINE sweep2D_prodquad_P0_vectoripol3_2(sweeper,i)
       CLASS(sweeperType),INTENT(INOUT) :: sweeper
@@ -771,18 +769,10 @@ MODULE openmp
       DO iang=sweeper%modRayDat%iangstt,sweeper%modRayDat%iangstp
         wtangazi = sweeper%modRayDat%angles(iang)%dlr* &
           sweeper%modRayDat%angquad%walpha(iang)*PI
-!         DO ipol=1,npol
-!           wtang(ipol) = wtangazi*sweeper%modRayDat%angquad%wtheta(ipol)* &
-!             sweeper%modRayDat%angquad%sinpolang(ipol)
-!         ENDDO !ipol
 
         !vectorizing the first block here
         wtang = wtangazi*sweeper%modRayDat%angquad%wtheta* &
           sweeper%modRayDat%angquad%sinpolang
-
-!         !variation 1: including the bounds
-!         wtang(1:npol) = wtangazi*sweeper%modRayDat%angquad%wtheta(1:npol)* &
-!           sweeper%modRayDat%angquad%sinpolang(1:npol)
 
         phibar = 0.0D0
 
@@ -912,8 +902,8 @@ MODULE openmp
       npol = SIZE(sweeper%modRayDat%angquad%wtheta)
 
       wsum = 4.0D0*PI
-      nproc=32
-PRINT*,sweeper%longRayDat%nlongrays
+      nproc=8
+! PRINT*,sweeper%longRayDat%nlongrays
 
       ifrstreg_proc = sweeper%myModMesh%ifrstfsreg(sweeper%imeshstt)
       ALLOCATE(tphi(sweeper%nreg,nproc))
@@ -1038,7 +1028,7 @@ PRINT*,sweeper%longRayDat%nlongrays
 
       !Reduce over threads
       DO ithd=1,nproc
-         PRINT*,ithd,nproc
+!          PRINT*,ithd,nproc
         sweeper%phis1g=sweeper%phis1g+tphi(:,ithd)
       ENDDO
 
@@ -1046,173 +1036,6 @@ PRINT*,sweeper%longRayDat%nlongrays
         sweeper%qbar*wsum
       DEALLOCATE(tphi)
     ENDSUBROUTINE sweep2D_prodquad_P0_vectoripol3_3
-! !===============================================================================
-!     !Based on the previous performances.
-!     !This version tried to include omp
-!     SUBROUTINE sweep2D_prodquad_P0_vectoripol3_4(sweeper,i)
-!       CLASS(sweeperType),INTENT(INOUT) :: sweeper
-!       INTEGER,INTENT(IN) :: i
-
-!       ! Local Variables
-!       INTEGER :: iang,ipol,ilray,imray,imod,im,iside,inextsurf,ifrstreg
-!       INTEGER :: imseg,iseg,iseg1,iseg2,ibc1,ibc2,nseglray,is1,is2,npol,ireg
-!       INTEGER :: irg_seg(0:sweeper%maxsegray),ithd
-!       INTEGER :: ifrstreg_proc,ireg1,ireg2
-!       DOUBLE PRECISION,ALLOCATABLE :: phid1(:),phid2(:)
-!       DOUBLE PRECISION :: wsum,rpol
-!       DOUBLE PRECISION :: wtangazi,wtang(SIZE(sweeper%modRayDat%angquad%wtheta))
-!       DOUBLE PRECISION,ALLOCATABLE :: phio1(:,:),phio2(:,:)
-!       DOUBLE PRECISION,ALLOCATABLE :: tphi(:)
-!       DOUBLE PRECISION :: tau_seg(sweeper%maxsegray)
-!       DOUBLE PRECISION :: &
-!         exparg(SIZE(sweeper%modRayDat%angquad%wtheta),sweeper%maxsegray)
-!       DOUBLE PRECISION,ALLOCATABLE :: phibar(:,:)
-!       TYPE(LongRayType_Base) :: ilongRay
-
-!       DOUBLE PRECISION :: timerStt,timerStp,temp
-!       DOUBLE PRECISION,ALLOCATABLE :: arg(:)
-
-!       DOUBLE PRECISION :: xval
-!       INTEGER :: ix,OMP_GET_THREAD_NUM,nproc
-!       DOUBLE PRECISION :: OMP_GET_WTIME,oStt,oStp
-
-!       ithd = 1
-!       npol = SIZE(sweeper%modRayDat%angquad%wtheta)
-
-!       wsum = 4.0D0*PI
-!       nproc=1
-
-!       ifrstreg_proc = sweeper%myModMesh%ifrstfsreg(sweeper%imeshstt)
-!       ALLOCATE(tphi(sweeper%nreg))
-!       CALL CPU_TIME(timerStt)
-!       oStt=OMP_GET_WTIME()
-
-! ! OPEN(FILE='a.out',UNIT=456)
-
-! !$OMP PARALLEL NUM_THREADS(nproc) DEFAULT(PRIVATE) &
-! !$OMP SHARED(sweeper,npol,wsum,ifrstreg_proc,expoa,expob,PI)
-! !$    ithd=OMP_GET_THREAD_NUM()+1
-!       ALLOCATE(phid1(npol))
-!       ALLOCATE(phid2(npol))
-!       ALLOCATE(phio1(npol,0:sweeper%maxsegray))
-!       ALLOCATE(phio2(npol,1:sweeper%maxsegray+1))
-
-!       ALLOCATE(phibar(npol,sweeper%nreg))
-!       tphi = 0.0D0
-
-!       DO iang=sweeper%modRayDat%iangstt,sweeper%modRayDat%iangstp
-!         wtangazi = sweeper%modRayDat%angles(iang)%dlr* &
-!           sweeper%modRayDat%angquad%walpha(iang)*PI
-!         !vectorizing the first block here
-!         wtang = wtangazi*sweeper%modRayDat%angquad%wtheta* &
-!           sweeper%modRayDat%angquad%sinpolang
-
-!         phibar = 0.0D0
-
-! !$OMP DO SCHEDULE(DYNAMIC,4)
-!         DO ilray=1,sweeper%longRayDat%nlongrays(iang)
-!           ilongRay = sweeper%longRayDat%angles(iang)%longrays(ilray)
-!           im = ilongRay%ifirstModMesh
-!           iside = ilongRay%iside(1)
-!           imray = ilongRay%firstModRay
-!           ibc1 = ilongRay%BCIndex(1)
-!           ibc2 = ilongRay%BCIndex(2)
-!           is1 = ilongRay%iside(1)
-!           is2 = ilongRay%iside(2)
-!           iseg = 0
-
-!           DO imod=1,ilongRay%nmods
-!             ifrstreg = sweeper%myModMesh%ifrstfsreg(im)
-
-!             DO imseg=1,sweeper%rtmesh(im)%rtdat%angles(iang)%rays(imray)%nseg
-!               ireg = ifrstreg - ifrstreg_proc + &
-!                 sweeper%rtmesh(im)%rtdat%angles(iang)%rays(imray)%ireg(imseg)
-!               iseg = iseg + 1
-!               tau_seg(iseg) = -sweeper%xstr(ireg)* &
-!                 sweeper%rtmesh(im)%rtdat%angles(iang)%rays(imray)%hseg(imseg)
-!               irg_seg(iseg) = ireg
-!             ENDDO !imseg
-!             inextsurf = sweeper%modRayDat%angles(iang)%rays(imray)%nextsurf(1)
-!             imray = sweeper%modRayDat%angles(iang)%rays(imray)%nextray(1)
-!             im = sweeper%myModMesh%neigh(inextsurf,im)
-!           ENDDO !imod
-
-!           nseglray = iseg
-!           !Evaluate exponential function
-!           DO iseg=1,nseglray
-!             xval=tau_seg(iseg)*1000.0D0
-!             ix=INT(xval)
-!             ix=MAX(ix,-16000)
-!             exparg(:,iseg)=expoa(:,ix)*xval+expob(:,ix)
-!           ENDDO
-
-
-!           phio1(:,0) = &
-!             sweeper%phiang1g_in%angle(iang)%face(is1)%angflux(:,ibc1)
-!           phio2(:,nseglray+1) = &
-!             sweeper%phiang1g_in%angle(iang)%face(is2)%angflux(:,ibc2)
-!           iseg2 = nseglray + 1
-
-!           DO iseg1=1,nseglray
-!             iseg2 = iseg2 - 1
-
-!             ireg1 = irg_seg(iseg1)
-!             phid1 = phio1(:,iseg1-1) - sweeper%qbar(ireg1)
-!             phid1 = phid1*exparg(:,iseg1)
-!             !phio1 stores the outgoing angular flux to be used for the next
-!             !segment as incoming angular flux.
-!             phio1(:,iseg1) = phio1(:,iseg1-1) - phid1
-!             phibar(:,ireg1) = phibar(:,ireg1) + phid1*wtang
-
-!             ireg2 = irg_seg(iseg2)
-!             phid2 = phio2(:,iseg2+1) - sweeper%qbar(ireg2)
-!             phid2 = phid2*exparg(:,iseg2)
-!             !phio1 stores the outgoing angular flux to be used for the next
-!             !segment as incoming angular flux.
-!             phio2(:,iseg2) = phio2(:,iseg2+1) - phid2
-!             phibar(:,ireg2) = phibar(:,ireg2) + phid2*wtang
-!           ENDDO !iseg
-!           sweeper%phiang1g_out%angle(iang)%face(is1)%angflux(:,ibc1) = &
-!             phio2(:,1)
-!           sweeper%phiang1g_out%angle(iang)%face(is2)%angflux(:,ibc2) = &
-!             phio1(:,nseglray)
-!         ENDDO !ilray
-! !$OMP END DO NOWAIT
-
-!         !This is to sum over the azimuthal angles owned by the current proc
-!         !MPACT says it's for polar angles, which I think is not true.
-!         tphi = tphi + SUM(phibar,DIM=1)
-
-! !$OMP BARRIER
-
-! !$OMP SINGLE
-!         CALL sweeper%UpdateBC%Start(iang,sweeper%phiang1g_out,sweeper%phiang1g_in)
-! !$OMP END SINGLE
-!       ENDDO !iang
-
-
-!       DEALLOCATE(phibar)
-!       DEALLOCATE(phio1)
-!       DEALLOCATE(phio2)
-!       DEALLOCATE(phid1)
-!       DEALLOCATE(phid2)
-! !$OMP END PARALLEL
-
-!       CALL CPU_TIME(timerStp)
-!       oStp=OMP_GET_WTIME()
-!       WRITE(*,*) "sweep time: ", timerStp-timerStt
-!       WRITE(*,*) "sweep wall time: ", oStp-oStt
-
-!       !Reduce over threads
-!       DO ithd=1,nproc
-!          PRINT*,ithd,nproc
-!         sweeper%phis1g=sweeper%phis1g+tphi(:,ithd)
-!       ENDDO
-
-!       sweeper%phis1g = sweeper%phis1g/(sweeper%xstr*sweeper%vol/sweeper%pz) + &
-!         sweeper%qbar*wsum
-!       DEALLOCATE(tphi)
-!     ENDSUBROUTINE sweep2D_prodquad_P0_vectoripol3_4
 
 
 ENDMODULE openmp
